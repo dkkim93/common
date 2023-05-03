e1f3e68bcd219cd137d5fab454c5e86e2468ee50
Dong-Ki Kim <dkkim93@mit.edu>
2023-05-03 15:09:08 -0400

    added bs utils

---
+++ common/beautifulsoup.py
@@ -0,0 +1,19 @@
+# define list of interactable tags
+interactable_tags = [
+    "a",
+    "button",
+    "details",
+    "embed",
+    "input",
+    "label",
+    "select",
+    "textarea",
+    "yt-formatted-string"
+]
+
+
+# define list of extractable tags
+extractable_tags = [
+    "div",
+    "ytd-transcript-segment-renderer"
+]
